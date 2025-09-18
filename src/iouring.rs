9d6bb86c2d9bd7317ddce18d85e459e41ff91067
sugarraysam <samuel.blaisdowdy@protonmail.com>
2025-09-18 15:10:52 -0700

    Implement Worker.

--- src/iouring.rs
+++ src/iouring.rs
@@ -1,3 +1,5 @@
 pub mod types;
 
 pub mod ring;
+
+pub mod worker;
--- src/iouring/types.rs
+++ src/iouring/types.rs
@@ -1,15 +1,12 @@
+use io_uring::squeue::Entry;
+use io_uring::{opcode, types};
 use std::os::unix::io::RawFd;
 
-// Create my own types, independent of `io_uring::opcode` because want to
-// abstract away and maybe be able to move away. Will also pass less data on
-// channel.
-// Write the enum itself on channel, then do pattern matching on other side and
-// explode to `io_uring::opcode` and then SQE with `<opcode>.build()`
-
-// Model supported IO operations based on liburing as source-of-truth: https://github.com/axboe/liburing/blob/master/src/include/liburing.h
-// Find `io_uring_prep_**`
-#[derive(Debug, Clone)]
+// Model supported IO operations based on liburing as source-of-truth:
+// https://github.com/axboe/liburing/blob/master/src/include/liburing.h
+#[derive(Debug, Clone, Default)]
 pub enum IoBase {
+    #[default]
     Nop,
     Read {
         fd: RawFd,
@@ -23,4 +20,49 @@ pub enum IoBase {
         nbytes: u32,
         offset: u64,
     },
+}
+
+impl IoBase {
+    pub fn into_entry(self) -> Entry {
+        match self {
+            IoBase::Nop => opcode::Nop::new().build(),
+
+            IoBase::Read {
+                fd,
+                buf,
+                nbytes,
+                offset,
+            } => opcode::Read::new(types::Fd(fd), buf, nbytes)
+                .offset(offset)
+                .build(),
+
+            IoBase::Write {
+                fd,
+                buf,
+                nbytes,
+                offset,
+            } => opcode::Write::new(types::Fd(fd), buf, nbytes)
+                .offset(offset)
+                .build(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SqeWrapper {
+    entry: Entry,
+}
+
+impl SqeWrapper {
+    pub fn reset(&mut self, io: IoBase) {
+        self.entry = io.into_entry();
+    }
+}
+
+impl Default for SqeWrapper {
+    fn default() -> Self {
+        let entry = IoBase::default().into_entry();
+
+        Self { entry }
+    }
 }
